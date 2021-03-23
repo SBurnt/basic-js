5a0888f094bf5cd304e62440de42564e4ca2bb00
SBurnt <zip2004zip@gmail.com>
2021-03-23 16:35:34 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(mas) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let res = [];
+  if (Array.isArray(mas)) {
+    mas.filter((el) => {
+      if (typeof el == 'string') {
+        let newEl = el.trim().toUpperCase();
+        res.push(newEl[0]);
+      }
+    });
+    console.log('resSort ', res.sort().join(''));
+    return res.sort().join('');
+  } else {
+    return false;
+  }
 };
